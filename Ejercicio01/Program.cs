c57de4876b28e712205a3c8e3d38678921cf19d2
alumno <alumno>
2017-03-23 19:01:27 -0300

    Guia de ejercicios

---
+++ Ejercicio01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    /*Ingresar 5 números por consola, guardándolos en una variable
+    escalar. Luego calcular y mostrar: el valor máximo, el valor
+    mínimo y el promedio.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, maximo = 0, minimo = 0, contador = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Ingrese un numero: ");
+                
+                while (!(int.TryParse(Console.ReadLine(), out num)))
+                {
+                    Console.WriteLine("Error. Reingrese: ");
+                }
+
+                if (i == 0)
+                {
+                    maximo = num;
+                    minimo = num;
+                }
+                else {
+                    if (num > maximo)
+                        maximo = num;
+                    if (num < minimo)
+                        minimo = num;
+                }
+
+                contador = contador + num;
+            }
+
+            Console.WriteLine("Valor maximo: {0}\nValor minimo: {1}\nValor promedio: {2}", maximo, minimo, contador / 5);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio09/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio09
+{
+    /* Escribir un programa que imprima por pantalla una pirámide como
+    la siguiente:
+    *
+    ***
+    *****
+    *******
+    *********    
+    El usuario indicará cuál será la altura de la pirámide ingresando
+    un número entero positivo. Para el ejemplo anterior la altura
+    ingresada fue de 5.
+    Nota: Utilizar estructuras repetitivas y selectivas.*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 5;
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            for (int i = 0; i < length; i++)
+            {                
+                Console.Write("*");
+                    
+                if(i != 0)
+                {
+                    for (int j = 0; j < i*2; j++)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();   
+        }
+    }
+}
