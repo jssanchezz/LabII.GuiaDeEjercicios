436c77027a7416e3db039e812559aeaab748c5e3
alumno <alumno>
2017-03-23 22:16:31 -0300

    Ejercicio de Clases

---
+++ Clase03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Seteando e imprimiendo
+            Sello.mensaje = "Holaaaaaaa";
+            Console.WriteLine(Sello.Imprimir());
+
+            //Borro e imprimo
+            Sello.Borrar();
+            Console.WriteLine(Sello.Imprimir());
+
+            //Seteo e imprimo
+            Sello.mensaje = "Chau";
+            Console.WriteLine(Sello.Imprimir());
+
+            //Probando con colores
+
+            Sello.color = ConsoleColor.Cyan;
+            Sello.mensaje = "Estoy en color!";
+            Sello.ImprimirEnColor();
+
+            Sello.color = ConsoleColor.Red ;
+            Sello.mensaje = "En otro color!";
+            Sello.ImprimirEnColor();
+            Console.WriteLine(Sello.Imprimir());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase03/Sello.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase03
+{
+    public class Sello
+    {
+        #region Atributos
+
+        public static string mensaje;
+        public static ConsoleColor color;
+
+        #endregion
+
+        #region Métodos
+
+        public static string Imprimir()
+        {
+            if (mensaje == null)
+                return null;
+            else
+                return ArmarMensaje();
+        }
+
+        public static void Borrar()
+        {
+            Sello.mensaje = null;
+        }
+
+        public static void ImprimirEnColor()
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(Imprimir());
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        private static string ArmarMensaje()
+        {
+            
+            string aux = null;
+
+            for (int i = 0; i < mensaje.Length+2; i++)
+            {
+                aux += "*";
+            }
+
+            aux += "\n*" + mensaje + "*\n";
+
+            for (int i = 0; i < mensaje.Length+2; i++)
+            {
+                aux += "*";
+            }
+
+            return aux;
+        }
+
+        #endregion
+    }
+}
